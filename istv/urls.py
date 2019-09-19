0950a480af0850a77d01153948ce2a8a33326b11
Joe Molloy <joe.m34@gmail.com>
2019-09-19 12:01:56 +0200

    aws stuff

--- istv/urls.py
+++ istv/urls.py
@@ -20,5 +20,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('user/', include('django.contrib.auth.urls')), # new
     path('app/', include('skitipp.urls')),
+    path('/', 'app/racelist/'),
 ]
 
