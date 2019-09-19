6116cf07b15da2b5619577da21c40fab9c5ca5eb
Joe Molloy <joe.m34@gmail.com>
2019-09-19 16:00:27 +0200

    password for initial user

--- skitipp/management/commands/createsu.py
+++ skitipp/management/commands/createsu.py
@@ -1,8 +1,13 @@
 from django.core.management.base import BaseCommand
 from django.contrib.auth.models import User
 
+import os
+
 class Command(BaseCommand):
 
     def handle(self, *args, **options):
+
+        initial_password = os.environ.get('INITIAL_ISTV_SUPER_USER_PASSWORD', "password")
+
         if not User.objects.filter(username="joemolloy").exists():
-            User.objects.create_superuser("joemolloy", "joe.m34@gmail.com", "admin")
+            User.objects.create_superuser("joemolloy", "joe.m34@gmail.com", initial_password,"admin")
