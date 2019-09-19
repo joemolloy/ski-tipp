c1a0a65c8ae4e250cf998b15dcab383a4e0e6512
Joe Molloy <joe.m34@gmail.com>
2019-09-19 11:59:27 +0200

    database, new url for aws

--- istv/settings.py
+++ istv/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
 DEBUG = os.environ.get('DJANGO_DEBUG', "True")
 
 
-ALLOWED_HOSTS = ['localhost', 'django-env.hjm7cexy84.us-west-2.elasticbeanstalk.com', 'skitipp.org']
+ALLOWED_HOSTS = ['localhost', 'istv.hjm7cexy84.us-west-2.elasticbeanstalk.com', 'skitipp.org']
 
 LOGIN_URL = '/user/login'
 LOGIN_REDIRECT_URL = '/app/racelist/'
